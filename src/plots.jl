9e521b9916c5bf6c262ff482a5376ceb1988ba1e
Orestis Ousoultzoglou <orousoultzoglou@gmail.com>
2023-09-11 01:29:20 +0300

    Use indexing instead of subplot arg in plot

--- src/plots.jl
+++ src/plots.jl
@@ -1,9 +1,9 @@
 function plot_histories(groundtruth_state_history, state_estimation_history)
-    plot(layout = (7, 1), size = (600, 1200))
+    p = plot(layout = (7, 1), size = (600, 1200))
 
     for i in 1:7
-        plot!(subplot = i, groundtruth_state_history[i, :], label = "Ground Truth", xlabel = "Timestep", ylabel = "Value", linewidth = 2)
-        plot!(subplot = i, state_estimation_history[i, :], label = "Estimated", xlabel = "Timestep", ylabel = "Value", linewidth = 2)
+        plot!(p[i], groundtruth_state_history[i, :], label = "Ground Truth", xlabel = "Timestep", ylabel = "Value", linewidth = 2)
+        plot!(p[i], state_estimation_history[i, :], label = "Estimated", xlabel = "Timestep", ylabel = "Value", linewidth = 2)
     end
 
     display(current())
