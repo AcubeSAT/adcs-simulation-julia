d9176a1526d8514da50f494322e163d8f4dcecd3
Orestis Ousoultzoglou <orousoultzoglou@gmail.com>
2023-09-20 21:54:10 +0300

    Add exports; include RW, control, noise models

--- src/ADCSSims.jl
+++ src/ADCSSims.jl
@@ -9,13 +9,20 @@ using Plots
 using Statistics
 
 include("quaternion.jl")
+include("reaction_wheel.jl")
+include("control.jl")
+include("noise_models.jl")
 include("dynamics.jl")
 include("losses.jl")
 include("utils.jl")
 include("parameters.jl")
 include("mekf.jl")
-include("noise_models.jl")
 include("simulation.jl")
 include("plots.jl")
+
+export Quaternion, scalar, vector, rotvec, toeuler
+export ReactionWheel, stribeck, deadzone_compensation, saturation_compensation
+export PDController, calculate_torque
+export mse, qloss
 
 end
