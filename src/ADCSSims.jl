b8f72f57817b7b8cf2df16d1312381673a446b6d
Orestis Ousoultzoglou <orousoultzoglou@gmail.com>
2023-09-25 01:42:48 +0300

    Add Quaternion exports

--- src/ADCSSims.jl
+++ src/ADCSSims.jl
@@ -26,7 +26,7 @@ include("mekf.jl")
 include("simulation.jl")
 include("plots.jl")
 
-export Quaternion
+export Quaternion, QuaternionF16, QuaternionF32, QuaternionF64
 export NadirSensor, StarTracker, SunSensor, in_fov, available, qerr, estimateq
 export ReactionWheel, stribeck, deadzone_compensation, saturation_compensation
 export PDController, calculate_torque, decompose_torque
