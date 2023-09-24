4acf5abd34f56fc1213e8edbbbf6b531deda6ff6
Orestis Ousoultzoglou <orousoultzoglou@gmail.com>
2023-09-24 03:04:39 +0300

    Remove abstract Quaternion type, specialize on Real subtypes

--- src/quaternion.jl
+++ src/quaternion.jl
@@ -1,39 +1,37 @@
-# Inspired by @moble and Base.Complex
+# Inspired by Michael Boyle and Base.Complex
 
 # Signify standard arithmetic operations should be implemented on it
-abstract type AbstractQuaternion{T<:Number} <: Number end
-
-struct Quaternion{T<:Number} <: AbstractQuaternion{T}
+struct Quaternion{T<:Real} <: Number
     coeffs::SVector{4,T}
-    Quaternion{T}(v::SVector{4,T}) where {T<:Number} = new{T}(v)
+    Quaternion{T}(v::SVector{4,T}) where {T<:Real} = new{T}(v)
     # Always use SVector internally
-    Quaternion{T}(v::A) where {T<:Number, A<:AbstractVector} = new{T}(SVector{4,T}(v))
+    Quaternion{T}(v::A) where {T<:Real, A<:AbstractVector} = new{T}(SVector{4,T}(v))
 end
-
-# Given a numeric type, return a QT type (not instance) specialized on T
-(::Type{QT})(::Type{T}) where {T<:Number, QT<:AbstractQuaternion} = QT{T}
-# Given a QT specialized on T, return another QT also specialized on T
-(::Type{QT})(::Type{<:AbstractQuaternion{T}}) where {T<:Number, QT<:AbstractQuaternion} = QT{T}
 
 # Construct without having to specify T
-Quaternion(v::SVector{4,T}) where {T<:Number} = Quaternion{T}(v)
-Quaternion(v::AbstractVector{T}) where {T<:Number} = Quaternion{T}(v)
+Quaternion(v::SVector{4,T}) where {T<:Real} = Quaternion{T}(v)
+Quaternion(v::AbstractVector{T}) where {T<:Real} = Quaternion{T}(v)
 
 # Explicitly state type, use SVector promote_rules for mixed types
-(::Type{QT})(w, x, y, z) where {T<:Number, QT<:AbstractQuaternion{T}} = QT(SVector{4,T}(w, x, y, z))
+(::Type{Quaternion{T}})(w, x, y, z) where {T<:Real} = Quaternion(SVector{4,T}(w, x, y, z))
 # Rely on type inference, use SVector promote_rules for mixed types
-function (::Type{QT})(w, x, y, z) where {QT<:AbstractQuaternion}
+function (::Type{Quaternion})(w, x, y, z)
     v = SVector{4}(w, x, y, z)
-    return QT{eltype(v)}(v)
+    return Quaternion{eltype(v)}(v)
 end
 
-coeffs(q::AbstractQuaternion) = getfield(q, :coeffs)
+coeffs(q::Quaternion) = getfield(q, :coeffs)
 
 # Type-preserving copy constructor
-(::Type{QT})(q::QT) where {T<:Number, QT<:AbstractQuaternion{T}} = QT(coeffs(q)...)
+Quaternion{T}(q::Quaternion{T}) where {T<:Real} = Quaternion(coeffs(q)...)
 # Type conversion copy constructor
-(::Type{QT})(q::AbstractQuaternion{S}) where {T<:Number, S<:Number, QT<:AbstractQuaternion{T}} = QT(SVector{4, T}(coeffs(q)...))
+Quaternion{T}(q::Quaternion{S}) where {T<:Real, S<:Real} = Quaternion(SVector{4, T}(coeffs(q)...))
 
 const QuaternionF64 = Quaternion{Float64}
 const QuaternionF32 = Quaternion{Float32}
 const QuaternionF16 = Quaternion{Float16}
+
+# Given a numeric type, return a Quaternion (not instance) specialized on T
+(::Type{Quaternion})(::Type{T}) where {T<:Real} = Quaternion{T}
+# Given a Quaternion specialized on T, return another Quaternion also specialized on T
+(::Type{Quaternion})(::Type{Quaternion{T}}) where {T<:Real} = Quaternion{T}
