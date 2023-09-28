312b5243d26bb49ae41d26a1e8172e6e36c29dfb
Orestis Ousoultzoglou <orousoultzoglou@gmail.com>
2023-09-29 02:16:23 +0300

    Add example torque decomposition function

--- src/control.jl
+++ src/control.jl
@@ -8,3 +8,18 @@ function calculate_torque(PD::PDController, qtarget, qestimated, w, wtarget)
     werr = w - wtarget
     return -sign(real(qrel)) * PD.Kp * vec(qrel) - PD.Kd * werr
 end
+
+# FIXME: This is for a 3RW system! It is not applicable for AcubeSAT
+function decompose_torque(τ, b, msaturation)
+    τw = dot(τ, b) / dot(b, b) * b
+    τm = τ - τw
+    m = cross(b, τm) / dot(b, b)
+    mprime = m / norm(τm)
+    ksm = min(abs(msaturation / mprime[1]),
+        abs(msaturation / mprime[2]),
+        abs(msaturation / mprime[3]))
+    mtrue = ksm * mprime
+    τsm = ksm * cross(b, mprime)
+    τw = τ - τsm
+    return τw, τsm, mtrue
+end
