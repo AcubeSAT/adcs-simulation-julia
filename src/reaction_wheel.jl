f8c7f228efac8d28efc549068606d3af142acf8b
Orestis Ousoultzoglou <orousoultzoglou@gmail.com>
2023-09-29 02:21:58 +0300

    Correct sigmoids in compensations, broadcast over w

--- src/reaction_wheel.jl
+++ src/reaction_wheel.jl
@@ -7,26 +7,38 @@
     wdeadzonemid = wdeadzone + 10
     saturationα
     deadzoneα
-    maxtorque = 0.001
+    maxtorque = 0.005
     Fc = 0.001
     b = 0.0005
     Fs = 0.0005
     w0 = 1
 end
 
-function stribeck(RW::ReactionWheel)
-    τcoulomb = RW.Fc * sign(RW.w)
-    τviscous = RW.b * RW.w
-    τstribeck = RW.Fs * exp(-(RW.w / RW.w0)^2) * sign(RW.w)
+function stribeck(Fc, w, b, w0)
+    τcoulomb = Fc * sign(w)
+    τviscous = b * w
+    τstribeck = Fc * exp(-(w / w0)^2) * sign(w)
     return τcoulomb + τviscous + τstribeck
 end
 
+function stribeck(RW::ReactionWheel)
+    return stribeck.(RW.Fc, RW.w, RW.b, RW.w0)
+end
+
+function deadzone_compensation(w, wdeadzone, wdeadzonemid, deadzoneα, maxtorque)
+    abs(w) <= wdeadzone || return zero(w)
+    return -sign(w) * maxtorque / (1 + exp(-deadzoneα * (w - wdeadzonemid)))
+end
+
 function deadzone_compensation(RW::ReactionWheel)
-    abs(RW.w) <= RW.wdeadzone || return zero(RW.w)
-    return -sign(RW.w) * RW.maxtorque / (1 + exp(-RW.deadzoneα * (RW.w - RW.wdeadzonemid)))
+    return deadzone_compensation.(RW.w, RW.wdeadzone, RW.wdeadzonemid, RW.deadzoneα, RW.maxtorque)
+end
+
+function saturation_compensation(w, wmax, saturationα, wmaxmid, maxtorque)
+    wmax <= abs(w) || return zero(w)
+    return -sign(w) * maxtorque / (1 + exp(-saturationα * (w - wmaxmid)))
 end
 
 function saturation_compensation(RW::ReactionWheel)
-    RW.wmax <= abs(RW.w) || return zero(RW.w)
-    return -sign(RW.w) * RW.maxtorque / (1 + exp(-RW.saturationα * (RW.w - RW.wmaxmid)))
+    return saturation_compensation.(RW.w, RW.wmax, RW.saturationα, RW.wmaxmid, RW.maxtorque)
 end
