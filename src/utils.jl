060a9f8a590d86c98c8277b19d9db030b373a700
Orestis Ousoultzoglou <orousoultzoglou@gmail.com>
2023-10-05 18:52:05 +0300

    Generate subset of state util

--- src/utils.jl
+++ src/utils.jl
@@ -13,3 +13,24 @@ function package_weights(x)
 end
 
 _struct_to_vec(S) = [getfield(S, field) for field in fieldnames(typeof(S))]
+
+function split_into_parts(vec::Vector, n::Int)
+    len = length(vec)
+    size_per_part = div(len, n)
+
+    parts = Vector{typeof(vec)}(undef, n)
+    start_idx = 1
+
+    for i in 1:n-1
+        parts[i] = vec[start_idx:start_idx+size_per_part-1]
+        start_idx += size_per_part
+    end
+    parts[n] = vec[start_idx:end] # last part takes the remainder
+
+    return parts
+end
+
+function subvector(t::Tuple, start_idx, end_idx=:end)
+    end_idx = end_idx == :end ? length(first(t)) : end_idx
+    return Tuple(v[start_idx:end_idx] for v in t)
+end
