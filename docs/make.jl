a150cfbc5b13a2dd621d5bfe234a7ad254028744
Orestis Ousoultzoglou <xlxs4@protonmail.ch>
2023-11-03 12:12:14 +0200

    docs: Update make.jl w/ correct repo

--- docs/make.jl
+++ docs/make.jl
@@ -6,11 +6,11 @@ DocMeta.setdocmeta!(ADCSSims, :DocTestSetup, :(using ADCSSims); recursive=true)
 makedocs(;
     modules=[ADCSSims],
     authors="Orestis Ousoultzoglou <orousoultzoglou@gmail.com>, Romanos Voulgarakis <voulgarakisromanos@gmail.com>, and contributors",
-    repo="https://github.com/AcubeSAT/adcs-simulation-julia/blob/{commit}{path}#{line}",
+    repo="https://github.com/EMTech-AUTh/ADCSSims.jl/blob/{commit}{path}#{line}",
     sitename="ADCSSims.jl",
     format=Documenter.HTML(;
         prettyurls=get(ENV, "CI", "false") == "true",
-        canonical="https://AcubeSAT.github.io/adcs-simulation-julia",
+        canonical="https://EMTech-AUTh.github.io/ADCSSims.jl",
         edit_link="main",
         assets=String[],
     ),
@@ -20,6 +20,6 @@ makedocs(;
 )
 
 deploydocs(;
-    repo="github.com/AcubeSAT/adcs-simulation-julia",
+    repo="github.com/EMTech-AUTh/ADCSSims.jl",
     devbranch="main",
 )
