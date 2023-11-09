281e7284682391981c32194242b8928186ccbcb6
Orestis Ousoultzoglou <orousoultzoglou@gmail.com>
2023-11-09 13:11:56 +0200

    Add Optional type draft

---
+++ src/optional.jl
@@ -0,0 +1,17 @@
+struct Optional{T}
+    data::Union{Nothing,T}
+    Optional{T}(data::Union{Nothing,T}=nothing) where {T} = new{T}(data)
+end
+
+Base.getindex(s::Optional) = s.data
+function Base.isequal(a::Optional, b::Optional)
+    if a.data === nothing || b.data === nothing
+        return true
+    else
+        return isequal(a.data, b.data)
+    end
+end
+
+Base.convert(::Type{Optional{T}}, ::Nothing) where T = Optional{T}()
+Base.convert(::Type{Optional{T}}, x) where T = Optional{T}(convert(T, x))
+Base.convert(::Type{Optional{T}}, x::Optional) where T = convert(Optional{T}, x[])
