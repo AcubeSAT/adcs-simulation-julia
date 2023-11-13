e64cd130b8cb249a404287642ffab8e0fb1e800b
Panagiotis Karvounaris <karvounarispanagiotis@gmail.com>
2023-11-14 01:33:51 +0200

    Make some code adjustments and Solve compiling problem

--- src/disturbances.jl
+++ src/disturbances.jl
@@ -2,27 +2,25 @@
 #    This function calculates the residual magnetic torque
  
 #    Inputs:
-#      Area       - Satellite's area projected to the sun
-#      cosines    - Cosine of angle between body frame axes and orbit frame z-axis
-#      B_body     - Magnetic field expressed in body frame
- 
+#      cosines      - Cosine of angle between body frame axes and orbit frame z-axis
+#      B_body       - Magnetic field expressed in body frame
+#      Change: I removed the area input parameter
 #    Ouputs:
-#      tau_rm     - Residual magnetic torque
-#      rm         - Residual magnetic moment
+#      res_mag_tor  - Residual magnetic torque
+#      rm           - Residual magnetic moment
  
 #  ======================================================================== 
 
-function residual_magnetic_torque(Area, cosines, B_body)
+function residual_magnetic_torque(cosines, B_body)
     mean_rm_base = [0.05, 0.05, 0.05]
-    maximum_area = [0.034, 0.034, 0.01]
     
     sign_vector = sign.(-cosines)
+
+    rm = mean_rm_base + 0.1 * mean_rm_base .* sign_vector + 0.0005 * rand(3)
+
+    res_mag_tor = cross(rm, B_body)
     
-    rm = mean_rm_base .+ (0.005 .* (Area ./ maximum_area) .* sign_vector) .+ 0.0005 .* rand(3)
-    
-    tau_rm = cross(rm, B_body)
-    
-    return tau_rm, rm
+    return res_mag_tor, rm
 end
 
 
@@ -37,7 +35,7 @@ end
 #      cm            - Center of mass
  
 #    Ouputs:
-#      solar_tor     - Solar pressure torque
+#      solar_rad_tor - Solar pressure torque
 #      area          - Satellite's area projected to the sun
 #      cosines       - Cosine of the angles between the body frame axes of the satellite and the sun vector transformed into the body frame
 #    Change: what the cosines are
@@ -56,33 +54,34 @@ function solar_radiation_pressure(R_OB, sun_vector, cm)
     proj_Yb_Zo = dot([0 1 0], y_0) * y_0;           
     proj_Zb_Zo = dot([0 0 1], y_0) * y_0;
 
-    proj_Xb_XYo = [1 0 0] - proj_Xb_Zo;                 # Projection of each body frame axes unit vector to orbit frame x,y plane
-    proj_Yb_XYo = [0 1 0] - proj_Yb_Zo;
-    proj_Zb_XYo = [0 0 1] - proj_Zb_Zo;
+    # Change: Make this a vector, not a matrix
+    proj_Xb_XYo = [1, 0, 0] - proj_Xb_Zo;                 # Projection of each body frame axes unit vector to orbit frame x,y plane
+    proj_Yb_XYo = [0, 1, 0] - proj_Yb_Zo;
+    proj_Zb_XYo = [0, 0, 1] - proj_Zb_Zo;
 
     # Change: use 0.34 and 0.1 instead of 0.034 and 0.01
     Ax = 0.34 * norm(cross(proj_Yb_XYo, proj_Zb_XYo)); # Surface projections to orbit frame x,y plane
     Ay = 0.34 * norm(cross(proj_Xb_XYo, proj_Zb_XYo));
     Az = 0.1 * norm(cross(proj_Xb_XYo, proj_Yb_XYo));  
     area = [Ax Ay Az];
-
+    
     cos_Xb_Xo = [1 0 0] * y_0;
     cos_Yb_Yo = [0 1 0] * y_0;
     cos_Zb_Zo = [0 0 1] * y_0;
     cosines = [cos_Xb_Xo, cos_Yb_Yo, cos_Zb_Zo]; 
 
-    solar_pressure_center = diag([0.05 0.05 0.17]); 
+    solar_pressure_center = Diagonal([0.05, 0.05, 0.17]); 
 
-    solar_pressure_center(1,:) = sign(-cos_Xb_Xo) * solar_pressure_center(1,:); 
-    solar_pressure_center(2,:) = sign(-cos_Yb_Yo) * solar_pressure_center(2,:); 
-    solar_pressure_center(3,:) = sign(-cos_Zb_Zo) * solar_pressure_center(3,:); 
+    solar_pressure_center[1,:] = sign.(-cos_Xb_Xo) .* solar_pressure_center[1,:]; 
+    solar_pressure_center[2,:] = sign.(-cos_Yb_Yo) .* solar_pressure_center[2,:]; 
+    solar_pressure_center[3,:] = sign.(-cos_Zb_Zo) .* solar_pressure_center[3,:]; 
 
-    # What about the cos(i) from the DDJF and why the use of cm
-    T1 = (Fs / c) * Ax * (1 + reflectance_factor) * cross(y_0, solar_pressure_center(1,:)' - cm);
-    T2 = (Fs / c) * Ay * (1 + reflectance_factor) * cross(y_0, solar_pressure_center(2,:)' - cm);
-    T3 = (Fs / c) * Az * (1 + reflectance_factor) * cross(y_0, solar_pressure_center(3,:)' - cm);
+    # Why do we use different type from the DDJF, the question is about the cross function and the cos(i) which does not exists
+    T1 = (Fs / c) * Ax * (1 + reflectance_factor) * cross(y_0, solar_pressure_center[1,:] - cm);
+    T2 = (Fs / c) * Ay * (1 + reflectance_factor) * cross(y_0, solar_pressure_center[2,:] - cm);
+    T3 = (Fs / c) * Az * (1 + reflectance_factor) * cross(y_0, solar_pressure_center[3,:] - cm);
 
-    solar_tor = (T1+T2+T3);
+    solar_rad_tor = (T1+T2+T3);
 
-    return solar_tor, area, cosines
+    return solar_rad_tor, area, cosines
 end
